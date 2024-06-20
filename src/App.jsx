7a5f92ad95f6d5878fdbfe660e9f9c62ac228abb
jovandimov <jovandimov@gmail.com>
2024-06-20 08:18:21 +0200

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import componenetsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
+
+//this is object destructuring...
+function CoreConcept({image,title,description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
@@ -10,7 +24,27 @@ function App() {
         </p>
       </header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>CoreConcepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />{" "}
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            />{" "}
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
